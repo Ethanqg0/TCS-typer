711f248606e04959cedca9dea6c6b4063ec071c5
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2024-05-24 18:00:31 -0700

    Test

--- netlify/functions/users/users.js
+++ netlify/functions/users/users.js
@@ -1,12 +1,10 @@
 const { createClient } = require("@supabase/supabase-js");
 
-
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
     "https://mjdbcmqftdfkonuolgbc.supabase.co/",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qZGJjbXFmdGRma29udW9sZ2JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzNDc0MDEsImV4cCI6MjAzMTkyMzQwMX0.v-PIg3KmT8etL81Eq5H7Tmt7xzH5ZtEc-NDSO-9mofs"
 );
-
 
 function sendSuccess(message) {
     return {
@@ -22,9 +20,7 @@ function sendError(message) {
 }
 
 const handler = async (event) => {
-
     try {
-
         console.log("Function init, fetching users...");
         const { data, error } = await supabase.from("users").select();
         if (error || !data) {

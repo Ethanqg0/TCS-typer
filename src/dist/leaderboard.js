503470eab29a11d0b4875e2246b709687019c611
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2024-05-24 17:54:10 -0700

    Bonehead?

--- src/dist/leaderboard.js
+++ src/dist/leaderboard.js
@@ -37,12 +37,13 @@ function filterBestTests(users) {
 window.addEventListener("DOMContentLoaded", function () {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const response = yield fetch("http://localhost:3000/users");
+            const response = yield fetch("https://tcs-typer-3.netlify.app/api/.netlify/functions/users");
             if (!response) {
                 throw new Error("Failed to fetch tests from backend server.");
             }
             let tests = yield response.json();
-            console.log(tests);
+            tests = tests["output"];
+            console.log("TESTS:", tests);
             tests = filterBestTests(tests);
             tests = rankByWPM(tests);
             for (let i = 0; i < tests.length; i++) {

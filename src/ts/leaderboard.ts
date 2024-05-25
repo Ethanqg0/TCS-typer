f70ec299f197f0b35a0d86b831e684ad3efbd12b
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2024-05-24 17:00:27 -0700

    Deployment: Changing deployment url to netlify

--- src/ts/leaderboard.ts
+++ src/ts/leaderboard.ts
@@ -30,7 +30,7 @@ function filterBestTests(users: Array<any>) {
 
 window.addEventListener("DOMContentLoaded", async function () {
   try {
-  const response = await fetch("https://tcs-typer.vercel.app/api/users");
+  const response = await fetch("https://tcs-typer-3.netlify.app/api/users");
     if (!response) {
       throw new Error("Failed to fetch tests from backend server.");
     }

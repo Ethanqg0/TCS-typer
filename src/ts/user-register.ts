db6d4781f4745ca08521e87f14e3f062f501f44d
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2024-05-28 16:45:50 -0700

    New Feature: Adding profanity checker

--- src/ts/user-register.ts
+++ src/ts/user-register.ts
@@ -1,6 +1,12 @@
+import Filter from "bad-words";
+
 let form: HTMLFormElement | null = null;
 
-function validateForm(username: HTMLInputElement, password: HTMLInputElement, verifyPassword: HTMLInputElement): boolean {
+function validateForm(
+  username: HTMLInputElement,
+  password: HTMLInputElement,
+  verifyPassword: HTMLInputElement
+): boolean {
   console.log("USERNAME VALUE:", username.value);
   if (!username.value.includes("tcswc")) {
     alert("Username must contain 'tcswc', use your Scratch login!");
@@ -14,19 +20,30 @@ function validateForm(username: HTMLInputElement, password: HTMLInputElement, ve
   return true;
 }
 
+function filterBadWords(text: string): void {
+  const filter = new Filter();
+  if (filter.isProfane(text)) {
+    throw new Error("Profanity is not allowed");
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const username = localStorage.getItem("username");
-  const usernameDisplay = document.getElementById("logged-username") as HTMLParagraphElement;
+  const usernameDisplay = document.getElementById(
+    "logged-username"
+  ) as HTMLParagraphElement;
   usernameDisplay.textContent = username || "";
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const logoutButton = document.getElementById("logout-button") as HTMLButtonElement;
+  const logoutButton = document.getElementById(
+    "logout-button"
+  ) as HTMLButtonElement;
 
   logoutButton?.addEventListener("click", () => {
-    window.localStorage.setItem("username", "")
-    window.location.reload()
-  })
+    window.localStorage.setItem("username", "");
+    window.location.reload();
+  });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -47,6 +64,15 @@ document.addEventListener("DOMContentLoaded", function () {
       const formResponse = validateForm(username, password, verifyPassword);
 
       if (formResponse === false) {
+        return;
+      }
+
+      try {
+        filterBadWords(username.value);
+        filterBadWords(fullName.value);
+      } catch (error) {
+        alert('We detected profanity. If we made a mistake, please let us know!');
+        console.error("Profanity detected in user input:", error as Error);
         return;
       }
 
@@ -75,7 +101,9 @@ document.addEventListener("DOMContentLoaded", function () {
           localStorage.setItem("username", username.value);
           window.location.href = "/";
         } else {
-          alert("An error occurred while registering the user. Does this user already exist?");
+          alert(
+            "An error occurred while registering the user. Does this user already exist?"
+          );
         }
       } catch (error) {
         console.error("An error occurred:", error);
@@ -87,44 +115,44 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector('#login-form') as HTMLFormElement;
-  const username = document.querySelector("#login-username") as HTMLInputElement;
-  const password = document.querySelector("#login-password") as HTMLInputElement;
+  const form = document.querySelector("#login-form") as HTMLFormElement;
+  const username = document.querySelector(
+    "#login-username"
+  ) as HTMLInputElement;
+  const password = document.querySelector(
+    "#login-password"
+  ) as HTMLInputElement;
 
   if (!form) {
     return;
   }
 
-  form.addEventListener('submit', async function (event) {
+  form.addEventListener("submit", async function (event) {
     event.preventDefault();
 
     try {
-      const response = await fetch(
-        "https://tcs-typer.netlify.app/api/login",
-        {
-          method: "POST",
-          mode: "cors",
-          cache: "no-cache",
-          credentials: "same-origin",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: username.value,
-            password: password.value,
-          }),
-        }
-      );
+      const response = await fetch("https://tcs-typer.netlify.app/api/login", {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username.value,
+          password: password.value,
+        }),
+      });
 
       if (response.ok) {
         localStorage.setItem("username", username.value);
-        window.location.href = '/';
+        window.location.href = "/";
       } else {
-        alert('An error occurred while logging in. Please try again.');
+        alert("An error occurred while logging in. Please try again.");
       }
     } catch (error) {
-      console.error('An error occurred:', error);
+      console.error("An error occurred:", error);
     }
   });
 });
-

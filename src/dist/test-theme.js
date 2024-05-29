e6877031c489cbc8bf382f59bb622ac72847eb94
Keaton Freed <keatonafreed2@icloud.com>
2024-05-29 15:27:02 -0700

    Dev: Made theme and sound change logic more dyanmic for later use.

--- src/dist/test-theme.js
+++ src/dist/test-theme.js
@@ -21,115 +21,33 @@ function changeClick(sound) {
     }
 }
 window.addEventListener("DOMContentLoaded", function () {
-    const standardClick = document.querySelector("#standard-click");
-    const mechanicalClick = document.querySelector("#mechanical-click");
-    const popClick = document.querySelector("#pop-click");
-    const clackyClick = document.querySelector("#clacky-click");
-    const capClick = document.querySelector("#cap-click");
-    standardClick.addEventListener("click", function () {
-        changeClick("standard-click");
-    });
-    mechanicalClick.addEventListener("click", function () {
-        changeClick("mechanical-click");
-    });
-    popClick.addEventListener("click", function () {
-        changeClick("pop-click");
-    });
-    clackyClick.addEventListener("click", function () {
-        changeClick("clacky-click");
-    });
-    capClick.addEventListener("click", function () {
-        changeClick("cap-click");
-    });
+    for (let sound of soundThemes) {
+        const soundButton = document.querySelector("#" + sound);
+        soundButton === null || soundButton === void 0 ? void 0 : soundButton.addEventListener("click", function () {
+            changeClick(sound);
+        });
+    }
 });
 window.addEventListener("DOMContentLoaded", function () {
     const body = document.querySelector("body");
-    let currentTheme = localStorage.getItem("theme");
     let currentSound = localStorage.getItem("sound");
-    if (currentSound === "standard-click") {
-        body === null || body === void 0 ? void 0 : body.classList.add("standard-click");
-    }
-    else if (currentSound === "mechanical-click") {
-        body === null || body === void 0 ? void 0 : body.classList.add("mechanical-click");
-    }
-    else if (currentSound === "pop-click") {
-        body === null || body === void 0 ? void 0 : body.classList.add("pop-click");
-    }
-    else if (currentSound === "clacky-click") {
-        body === null || body === void 0 ? void 0 : body.classList.add("clacky-click");
-    }
-    else if (currentSound === "cap-click") {
-        body === null || body === void 0 ? void 0 : body.classList.add("cap-click");
-    }
-    else {
+    let currentTheme = localStorage.getItem("theme");
+    if (!currentSound) {
         localStorage.setItem("sound", "standard-click");
-        body === null || body === void 0 ? void 0 : body.classList.add("standard-click");
+        currentSound = "standard-click";
     }
-    if (currentTheme === "matrix-theme") {
-        body === null || body === void 0 ? void 0 : body.classList.add("matrix-theme");
-    }
-    else if (currentTheme === "pink-theme") {
-        body === null || body === void 0 ? void 0 : body.classList.add("pink-theme");
-    }
-    else if (currentTheme === "discord-theme") {
-        body === null || body === void 0 ? void 0 : body.classList.add("discord-theme");
-    }
-    else if (currentTheme === "blueberry-theme") {
-        body === null || body === void 0 ? void 0 : body.classList.add("blueberry-theme");
-    }
-    else if (currentTheme === "dark-theme") {
-        body === null || body === void 0 ? void 0 : body.classList.add("dark-theme");
-    }
-    else if (currentTheme === "lavendar-theme") {
-        body === null || body === void 0 ? void 0 : body.classList.add("lavendar-theme");
-    }
-    else if (currentTheme === "forest-theme") {
-        body === null || body === void 0 ? void 0 : body.classList.add("forest-theme");
-    }
-    else if (currentTheme === "cartoon-theme") {
-        body === null || body === void 0 ? void 0 : body.classList.add("cartoon-theme");
-    }
-    else {
-        // If currentTheme is "default-theme" or null/undefined, set to "default-theme"
+    body === null || body === void 0 ? void 0 : body.classList.add(currentSound || "");
+    if (!currentTheme) {
         localStorage.setItem("theme", "default-theme");
-        body === null || body === void 0 ? void 0 : body.classList.add("default-theme");
+        currentTheme = "default-theme";
     }
+    body === null || body === void 0 ? void 0 : body.classList.add(currentTheme || "");
 });
 window.addEventListener("DOMContentLoaded", function () {
-    const defaultTheme = document.querySelector("#default-theme");
-    const darkTheme = document.querySelector("#dark-theme");
-    const lavendarTheme = document.querySelector("#lavendar-theme");
-    const forestTheme = document.querySelector("#forest-theme");
-    const matrixTheme = document.querySelector("#matrix-theme");
-    const pinkTheme = document.querySelector("#pink-theme");
-    const discordTheme = document.querySelector("#discord-theme");
-    const blueberryTheme = document.querySelector("#blueberry-theme");
-    const cartoonTheme = document.querySelector("#cartoon-theme");
-    defaultTheme.addEventListener("click", function () {
-        changeTheme("default-theme");
-    });
-    darkTheme.addEventListener("click", function () {
-        changeTheme("dark-theme");
-    });
-    lavendarTheme.addEventListener("click", function () {
-        changeTheme("lavendar-theme");
-    });
-    forestTheme.addEventListener("click", function () {
-        changeTheme("forest-theme");
-    });
-    cartoonTheme.addEventListener("click", function () {
-        changeTheme("cartoon-theme");
-    });
-    pinkTheme.addEventListener("click", function () {
-        changeTheme("pink-theme");
-    });
-    matrixTheme.addEventListener("click", function () {
-        changeTheme("matrix-theme");
-    });
-    discordTheme.addEventListener("click", function () {
-        changeTheme("discord-theme");
-    });
-    blueberryTheme.addEventListener("click", function () {
-        changeTheme("blueberry-theme");
-    });
+    for (let theme of colorThemes) {
+        const themeButton = document.querySelector("#" + theme);
+        themeButton === null || themeButton === void 0 ? void 0 : themeButton.addEventListener("click", function () {
+            changeTheme(theme);
+        });
+    }
 });
--- src/ts/test-theme.ts
+++ src/ts/test-theme.ts
@@ -23,121 +23,39 @@ function changeClick(sound: string) {
 }
 
 window.addEventListener("DOMContentLoaded", function () {
-  const standardClick = document.querySelector("#standard-click") as HTMLElement;
-  const mechanicalClick = document.querySelector("#mechanical-click") as HTMLElement;
-  const popClick = document.querySelector("#pop-click") as HTMLElement;
-  const clackyClick = document.querySelector("#clacky-click") as HTMLElement;
-  const capClick = document.querySelector("#cap-click") as HTMLElement;
-
-  standardClick.addEventListener("click", function () {
-    changeClick("standard-click");
-  });
-
-  mechanicalClick.addEventListener("click", function () {
-    changeClick("mechanical-click");
-  });
-
-  popClick.addEventListener("click", function () {
-    changeClick("pop-click");
-  });
-
-  clackyClick.addEventListener("click", function () {
-    changeClick("clacky-click");
-  });
-
-  capClick.addEventListener("click", function () {
-    changeClick("cap-click");
-  });
+  for (let sound of soundThemes) {
+    const soundButton = document.querySelector("#" + sound) as HTMLElement;
+    soundButton?.addEventListener("click", function () {
+      changeClick(sound);
+    });
+  }
 });
 
 
 window.addEventListener("DOMContentLoaded", function () {
   const body: HTMLElement | null = document.querySelector("body");
-  let currentTheme: string | null = localStorage.getItem("theme");
   let currentSound: string | null = localStorage.getItem("sound");
+  let currentTheme: string | null = localStorage.getItem("theme");
 
-  if (currentSound === "standard-click") {
-    body?.classList.add("standard-click");
-  } else if (currentSound === "mechanical-click") {
-    body?.classList.add("mechanical-click");
-  } else if (currentSound === "pop-click") {
-    body?.classList.add("pop-click");
-  } else if (currentSound === "clacky-click") {
-    body?.classList.add("clacky-click");
-  } else if (currentSound === "cap-click") {
-    body?.classList.add("cap-click");
-  } else {
+  if (!currentSound) {
     localStorage.setItem("sound", "standard-click");
-    body?.classList.add("standard-click");
+    currentSound = "standard-click"
   }
+  body?.classList.add(currentSound || "");
 
-  if (currentTheme === "matrix-theme") {
-    body?.classList.add("matrix-theme");
-  } else if (currentTheme === "pink-theme") {
-    body?.classList.add("pink-theme");
-  } else if (currentTheme === "discord-theme") {
-    body?.classList.add("discord-theme");
-  } else if (currentTheme === "blueberry-theme") {
-    body?.classList.add("blueberry-theme");
-  } else if (currentTheme === "dark-theme") {
-    body?.classList.add("dark-theme");
-  } else if (currentTheme === "lavendar-theme") {
-    body?.classList.add("lavendar-theme");
-  } else if (currentTheme === "forest-theme") {
-    body?.classList.add("forest-theme");
-  } else if (currentTheme === "cartoon-theme") {
-    body?.classList.add("cartoon-theme");
-  } else {
-    // If currentTheme is "default-theme" or null/undefined, set to "default-theme"
+
+  if (!currentTheme) {
     localStorage.setItem("theme", "default-theme");
-    body?.classList.add("default-theme");
+    currentTheme = "default-theme"
   }
+  body?.classList.add(currentTheme || "");
 });
 
 window.addEventListener("DOMContentLoaded", function () {
-  const defaultTheme = document.querySelector("#default-theme") as HTMLElement;
-  const darkTheme = document.querySelector("#dark-theme") as HTMLElement;
-  const lavendarTheme = document.querySelector("#lavendar-theme") as HTMLElement;
-  const forestTheme = document.querySelector("#forest-theme") as HTMLElement;
-  const matrixTheme = document.querySelector("#matrix-theme") as HTMLElement;
-  const pinkTheme = document.querySelector("#pink-theme") as HTMLElement;
-  const discordTheme = document.querySelector("#discord-theme") as HTMLElement;
-  const blueberryTheme = document.querySelector("#blueberry-theme") as HTMLElement;
-  const cartoonTheme = document.querySelector("#cartoon-theme") as HTMLElement;
-
-  defaultTheme.addEventListener("click", function () {
-    changeTheme("default-theme");
-  });
-
-  darkTheme.addEventListener("click", function () {
-    changeTheme("dark-theme");
-  });
-
-  lavendarTheme.addEventListener("click", function () {
-    changeTheme("lavendar-theme");
-  });
-
-  forestTheme.addEventListener("click", function () {
-    changeTheme("forest-theme");
-  });
-
-  cartoonTheme.addEventListener("click", function () {
-    changeTheme("cartoon-theme");
-  });
-
-  pinkTheme.addEventListener("click", function () {
-    changeTheme("pink-theme");
-  });
-
-  matrixTheme.addEventListener("click", function () {
-    changeTheme("matrix-theme");
-  });
-
-  discordTheme.addEventListener("click", function () {
-    changeTheme("discord-theme");
-  }); 
-
-  blueberryTheme.addEventListener("click", function () {
-    changeTheme("blueberry-theme");
-  });
+  for (let theme of colorThemes) {
+    const themeButton = document.querySelector("#" + theme) as HTMLElement;
+    themeButton?.addEventListener("click", function () {
+      changeTheme(theme);
+    });
+  }
 });

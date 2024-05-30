fa1cca6786e95d63c2db8f02083d5230996e1719
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2024-05-29 17:07:54 -0700

    Deployment: Adding environmental variables to backend functions

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -2,8 +2,8 @@ const { createClient } = require("@supabase/supabase-js");
 
 exports.handler = async function (event, context) {
   const supabase = createClient(
-    "https://mjdbcmqftdfkonuolgbc.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qZGJjbXFmdGRma29udW9sZ2JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzNDc0MDEsImV4cCI6MjAzMTkyMzQwMX0.v-PIg3KmT8etL81Eq5H7Tmt7xzH5ZtEc-NDSO-9mofs"
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
   );
 
   try {

313275b9d740b92cf8714d4d669ae3380cda6c41
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2024-08-03 10:56:32 -0700

    coaches leaderboard

--- src/ts/leaderboard.ts
+++ src/ts/leaderboard.ts
@@ -4,7 +4,6 @@ interface User {
   full_name: string;
   coach?: boolean;
   best_test: TestRecord;
-  // tests: Array<FilteredTest>;
 }
 
 interface TestRecord {
@@ -16,88 +15,99 @@ export interface BestTest {
   full_name?: string;
   wpm: number;
   accuracy: number;
+  isCoach: boolean;
 }
 
 function rankByWPM(tests: Array<BestTest>) {
-  const test = tests.sort((a, b) => b["wpm"] - a["wpm"]);
-  return test;
+  return tests.sort((a, b) => b.wpm - a.wpm);
 }
 
 function filterTests(users: Array<User>) {
   const filteredTests: Array<BestTest> = [];
 
   for (const user of users) {
-    if (user["coach"] === true) {
-      continue;
-    }
-    const test: TestRecord = user["best_test"];
+    const test: TestRecord = user.best_test;
     if (!test) continue;
     filteredTests.push({
-      full_name: user["full_name"],
-      wpm: test["wpm"],
-      accuracy: test["accuracy"],
-    } as BestTest);
+      full_name: user.full_name,
+      wpm: test.wpm,
+      accuracy: test.accuracy,
+      isCoach: user.coach ?? false,
+    });
   }
 
   return filteredTests;
 }
 
+// Make a coach leaderboard and student leaderboard that can be toggled
 (window as any).fetchAndDisplayLeaderboard = async () => {
   try {
     const response = await fetch(
       "https://tcs-typer.netlify.app/api/leaderboard"
-      // "http://localhost:8888/api/leaderboard"
     );
-    if (!response) {
+    if (!response.ok) {
       throw new Error("Failed to fetch tests from backend server.");
     }
 
     let tests = await response.json();
-
     tests = filterTests(tests);
-    tests = rankByWPM(tests);
+
+    const studentTests = rankByWPM(tests.filter((test) => !test.isCoach));
+    const coachTests = rankByWPM(tests.filter((test) => test.isCoach));
 
     const leaderboard = document.querySelector(
       ".flex-column"
     ) as HTMLDivElement;
-    leaderboard.innerHTML = "";
-    for (let i: number = 0; i < tests.length; i++) {
-      const player = leaderboard.appendChild(
-        document.createElement("div") as HTMLDivElement
-      );
-      player.classList.add("leaderboard-player");
+    const studentsLeaderboard = document.getElementById(
+      "leaderboard-toggle-students"
+    ) as HTMLButtonElement;
+    const coachesLeaderboard = document.getElementById(
+      "leaderboard-toggle-coaches"
+    ) as HTMLButtonElement;
 
-      const name = player.appendChild(
-        document.createElement("h3") as HTMLHeadingElement
-      );
+    function displayLeaderboard(tests: Array<BestTest>) {
+      leaderboard.innerHTML = "";
 
-      name.innerHTML = tests[i]["full_name"];
-      if (i === 0) {
-        name.innerHTML += " 🥇";
-        name.style.fontWeight = "600";
-      } else if (i === 1) {
-        name.innerHTML += " 🥈";
-      } else if (i === 2) {
-        name.innerHTML += " 🥉";
+      for (let i = 0; i < tests.length; i++) {
+        const player = leaderboard.appendChild(document.createElement("div"));
+        player.classList.add("leaderboard-player");
+
+        const name = player.appendChild(document.createElement("h3"));
+        name.innerHTML = tests[i].full_name ?? "";
+        if (i === 0) {
+          name.innerHTML += " 🥇";
+          name.style.fontWeight = "600";
+        } else if (i === 1) {
+          name.innerHTML += " 🥈";
+        } else if (i === 2) {
+          name.innerHTML += " 🥉";
+        }
+
+        const div = player.appendChild(document.createElement("div"));
+        div.classList.add("wpm-accuracy");
+
+        const wpm = div.appendChild(document.createElement("h4"));
+        wpm.innerHTML = `${tests[i].wpm}`;
+
+        const accuracy = div.appendChild(document.createElement("h4"));
+        accuracy.innerHTML = `${tests[i].accuracy}%`;
       }
-
-      const div = player.appendChild(
-        document.createElement("div") as HTMLDivElement
-      );
-      div.classList.add("wpm-accuracy");
-      const wpm = div.appendChild(
-        document.createElement("h4") as HTMLParagraphElement
-      );
-      wpm.innerHTML = tests[i]["wpm"];
-      const accuracy = div.appendChild(
-        document.createElement("h4") as HTMLParagraphElement
-      );
-      accuracy.innerHTML = tests[i]["accuracy"] + "%";
     }
+
+    studentsLeaderboard.addEventListener("click", () =>
+      displayLeaderboard(studentTests)
+    );
+    coachesLeaderboard.addEventListener("click", () =>
+      displayLeaderboard(coachTests)
+    );
+
+    // Display the student leaderboard by default
+    displayLeaderboard(studentTests);
   } catch (error) {
     console.error("An error occurred while fetching the leaderboard: ", error);
   }
 };
+
 
 window.addEventListener("DOMContentLoaded", async () => {
   // When the page is loaded, fetch and display the leaderboard
